983e6aad1181709cd552ddd2527535f66da3a8a8
xinleifds <1664294351@qq.com>
2026-08-04 09:53:29 +0800

    代码提交

---
+++ 2048/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+//TIP 要<b>运行</b>代码，请按 <shortcut.Ac actionId="Run"/> 或
+//// 点击装订区域中的 <icon src="AllIconstions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello World");
+    }
+}
